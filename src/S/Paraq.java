789e4f2d90d6aac29105330ff344ece30c5105ce
Alex <caroazulpavorangers@gmail.com>
2024-02-21 19:01:05 +0100

    NUEVO Con .gitignore y la primera funcionalidad

--- src/S/Paraq.java
+++ src/S/Paraq.java
@@ -5,6 +5,16 @@ public class Paraq {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int atun;
+
+while(atun) {
+	case 1: 
+		System.out.println("1");
+		
+	case 2:
+		System.out.println("2");
+
+}
+
 	}
 
 }
